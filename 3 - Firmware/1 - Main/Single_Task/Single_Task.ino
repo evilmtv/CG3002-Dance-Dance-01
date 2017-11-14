c84772218e95b7ad223ec38d6d455df7ff5f6a1b
Jun Hao <e0012616@u.nus.edu>
2017-11-14 22:05:15 +0800

    Added restart code, Cleaned up code

--- 3 - Firmware/1 - Main/Single_Task/Single_Task.ino
+++ 3 - Firmware/1 - Main/Single_Task/Single_Task.ino
@@ -2,19 +2,18 @@
 #include <Arduino_FreeRTOS.h>
 #include <task.h>
 
+#define SET_SIZE 40 // Number of sets of data for 1 sample size
+
+// Function Declarations
 void establishContact();
 void readSensors();
 void createMessage();
 
+// Variable Declarations
 TickType_t prevWakeTimeMain;
-TickType_t prevWakeTimeRead;
-
 int x0, y0, z0, x1, y1, z1, x2, y2, z2, x3, y3, z3, pwr;
-float sensorValue, current;
 char x0Char[4], y0Char[4], z0Char[4], x1Char[4], y1Char[4], z1Char[4], x2Char[4], y2Char[4], z2Char[4], x3Char[4], y3Char[4], z3Char[4], pwrChar[4];
 char messageStr[2500];
-
-#define SET_SIZE 40
 
 const int xpin0 = A0;
 const int ypin0 = A1;
@@ -40,7 +39,6 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Start up");
   establishContact();
-  //Serial.println("Finish contact");
   xTaskCreate(TaskMain, "TaskMain", 2500, NULL, 2, NULL);
 }
 
@@ -51,25 +49,21 @@ void loop()
 void TaskMain(void *pvParameters)
 {
   int readByte = 0;
-  int frameNum = 0;
+  unsigned int frameNum = 0;
   char frameNumChar[4];
   unsigned int len;
-  int sendFlag = 0;
-  int counter = 0;
   char checkSum = 0;
 
   prevWakeTimeMain = xTaskGetTickCount();
-  prevWakeTimeRead = xTaskGetTickCount();
 
   while (1)
   {
-    if (Serial.available()) {
-      readByte = Serial.read();
+    if (Serial.available()) {       // Check if message available
+      readByte = Serial.read();     // Read message received
     }
-    if (readByte == 'A') {
-      sendFlag = 0;
-      counter = 0;
+    if (readByte == 'A') {          // Requesting for new set of data
 
+      // addFrameNumber();
       itoa(frameNum, frameNumChar, 10);
       strcpy(messageStr, frameNumChar);
       strcat(messageStr, ",");
@@ -77,15 +71,13 @@ void TaskMain(void *pvParameters)
       for (int h = 0; h < SET_SIZE; h++) {
         readSensors();
         createMessage();
-        vTaskDelayUntil(&prevWakeTimeMain, (4 / portTICK_PERIOD_MS));
+        vTaskDelayUntil(&prevWakeTimeMain, (4 / portTICK_PERIOD_MS)); // Ensure one set read every 4ms
       }
 
+      // readPower(); addPowerToMessage();
       pwr = analogRead(pwrpin);
-      //sensorValue = ((float) pwr * 5)/1023;
+      //sensorValue = ((float) pwr * 5)/1023; // Declare sensorValue and current as float
       //current = sensorValue / (10/10.1);
-      //Serial.println(pwr);
-      //Serial.println(sensorValue);
-      //Serial.println(current);
       itoa(pwr, pwrChar, 10);
       strcat(messageStr, pwrChar);
       strcat(messageStr, ",");
@@ -103,8 +95,9 @@ void TaskMain(void *pvParameters)
         Serial.write(messageStr[j]);
       }
 
-      sendFlag = 1;
       frameNum++;
+
+      // Reset variables
       readByte = 0;
       checkSum = 0;
     }
@@ -114,45 +107,31 @@ void TaskMain(void *pvParameters)
         Serial.write(messageStr[k]);
       }
       readByte = 0;
-    }
-    else {
-      if (sendFlag)
-      {
-        counter++;
-        if (counter == 30000)
-        {
-          //Resend message
-          //for (int m = 0; m < len + 2; m++) {
-          //  Serial.write(messageStr[m]);
-          //}
-          counter = 0;
-        }
-      }
+    } else if (readByte == 'H') { // Reset
+      readByte = 0;
+      frameNum = 0;
+      checkSum = 0;
+      len = 0;
+      establishContact();
     }
   }
-  //vTaskDelayUntil(&prevWakeTimeMain, (100 / portTICK_PERIOD_MS));
 }
 
 
 void establishContact() {
   int flag = 0;
   int flag2 = 0;
-  while (flag == 0)
-  {
-    if (Serial.available())
-    {
-      if (Serial.read() == 'H')
-      {
+  while (flag == 0) {
+    if (Serial.available()) {
+      if (Serial.read() == 'H') {
         flag = 1;
         Serial.write('A');   // send a capital A
       }
     }
   }
   while (flag2 == 0) {
-    if (Serial.available())
-    {
-      if (Serial.read() == 'A')
-      {
+    if (Serial.available()) {
+      if (Serial.read() == 'A') {
         flag2 = 1;
       }
     } else {
@@ -163,32 +142,21 @@ void establishContact() {
 }
 
 void readSensors() {
-  //Reads accelerometers
+  //Read accelerometers
   x0 = analogRead(xpin0);
-  //delay(1);
   y0 = analogRead(ypin0);
-  //delay(1);
   z0 = analogRead(zpin0);
-  //delay(1);
 
   x1 = analogRead(xpin1);
-  //delay(1);
   y1 = analogRead(ypin1);
-  //delay(1);
   z1 = analogRead(zpin1);
-  //delay(1);
 
   x2 = analogRead(xpin2);
-  //delay(1);
   y2 = analogRead(ypin2);
-  //delay(1);
   z2 = analogRead(zpin2);
-  //delay(1);
 
   x3 = analogRead(xpin3);
-  //delay(1);
   y3 = analogRead(ypin3);
-  //delay(1);
   z3 = analogRead(zpin3);
 }
 
