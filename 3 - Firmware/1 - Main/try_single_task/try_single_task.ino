fef2bd7351c80087dace802e9a2facb08faee477
Jun Hao <e0012616@u.nus.edu>
2017-11-14 20:13:43 +0800

    Fixed Firmware code

--- 3 - Firmware/1 - Main/try_single_task/try_single_task.ino
+++ 3 - Firmware/1 - Main/try_single_task/try_single_task.ino
@@ -9,10 +9,10 @@ void createMessage();
 TickType_t prevWakeTimeMain;
 TickType_t prevWakeTimeRead;
 
-int x0, y0, z0, x1, y1, z1, x2, y2, z2, x3, y3, z3;
-int x0Rx, y0Rx, z0Rx, x1Rx, y1Rx, z1Rx, x2Rx, y2Rx, z2Rx, x3Rx, y3Rx, z3Rx;
-char x0Char[4], y0Char[4], z0Char[4], x1Char[4], y1Char[4], z1Char[4], x2Char[4], y2Char[4], z2Char[4], x3Char[4], y3Char[4], z3Char[4];
-char messageStr[1024];
+int x0, y0, z0, x1, y1, z1, x2, y2, z2, x3, y3, z3, pwr;
+float sensorValue, current;
+char x0Char[4], y0Char[4], z0Char[4], x1Char[4], y1Char[4], z1Char[4], x2Char[4], y2Char[4], z2Char[4], x3Char[4], y3Char[4], z3Char[4], pwrChar[4];
+char messageStr[2500];
 
 #define SET_SIZE 40
 
@@ -20,22 +20,24 @@ const int xpin0 = A0;
 const int ypin0 = A1;
 const int zpin0 = A2;
 
-const int xpin1 = A3;
-const int ypin1 = A4;
-const int zpin1 = A5;
+const int xpin1 = A5;
+const int ypin1 = A6;
+const int zpin1 = A7;
 
-const int xpin2 = A6;
-const int ypin2 = A8;
-const int zpin2 = A9;
+const int xpin2 = A8;
+const int ypin2 = A9;
+const int zpin2 = A10;
 
-const int xpin3 = A10;
-const int ypin3 = A11;
-const int zpin3 = A12;
+const int xpin3 = A13;
+const int ypin3 = A14;
+const int zpin3 = A15;
+
+const int pwrpin = A4;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
   Serial.println("Start up");
   establishContact();
   //Serial.println("Finish contact");
@@ -78,13 +80,23 @@ void TaskMain(void *pvParameters)
         vTaskDelayUntil(&prevWakeTimeMain, (4 / portTICK_PERIOD_MS));
       }
 
+      pwr = analogRead(pwrpin);
+      //sensorValue = ((float) pwr * 5)/1023;
+      //current = sensorValue / (10/10.1);
+      //Serial.println(pwr);
+      //Serial.println(sensorValue);
+      //Serial.println(current);
+      itoa(pwr, pwrChar, 10);
+      strcat(messageStr, pwrChar);
+      strcat(messageStr, ",");
+
       len = strlen(messageStr);
 
       for (int i = 0; i < len; i++) {
         checkSum ^= messageStr[i];
       }
       messageStr[len] = checkSum;
-      messageStr[len + 1] = '\n';
+      messageStr[len+1] = '\n';
 
       //Send message
       for (int j = 0; j < len + 2; j++) {
@@ -94,6 +106,7 @@ void TaskMain(void *pvParameters)
       sendFlag = 1;
       frameNum++;
       readByte = 0;
+      checkSum = 0;
     }
     else if (readByte == 'R') {
       //Resend message
@@ -109,20 +122,21 @@ void TaskMain(void *pvParameters)
         if (counter == 30000)
         {
           //Resend message
-          for (int m = 0; m < len + 2; m++) {
-            Serial.write(messageStr[m]);
-          }
+          //for (int m = 0; m < len + 2; m++) {
+          //  Serial.write(messageStr[m]);
+          //}
           counter = 0;
         }
       }
     }
   }
-  vTaskDelayUntil(&prevWakeTimeMain, (500 / portTICK_PERIOD_MS));
+  //vTaskDelayUntil(&prevWakeTimeMain, (100 / portTICK_PERIOD_MS));
 }
 
 
 void establishContact() {
   int flag = 0;
+  int flag2 = 0;
   while (flag == 0)
   {
     if (Serial.available())
@@ -130,85 +144,97 @@ void establishContact() {
       if (Serial.read() == 'H')
       {
         flag = 1;
+        Serial.write('A');   // send a capital A
       }
     }
   }
-  Serial.write('A');   // send a capital A
+  while (flag2 == 0) {
+    if (Serial.available())
+    {
+      if (Serial.read() == 'A')
+      {
+        flag2 = 1;
+      }
+    } else {
+      Serial.write('A');
+    }
+  }
+  Serial.write('\n');   // send a capital A
 }
 
 void readSensors() {
   //Reads accelerometers
   x0 = analogRead(xpin0);
-  delay(1);
+  //delay(1);
   y0 = analogRead(ypin0);
-  delay(1);
+  //delay(1);
   z0 = analogRead(zpin0);
-  delay(1);
+  //delay(1);
 
   x1 = analogRead(xpin1);
-  delay(1);
+  //delay(1);
   y1 = analogRead(ypin1);
-  delay(1);
+  //delay(1);
   z1 = analogRead(zpin1);
-  delay(1);
+  //delay(1);
 
   x2 = analogRead(xpin2);
-  delay(1);
+  //delay(1);
   y2 = analogRead(ypin2);
-  delay(1);
+  //delay(1);
   z2 = analogRead(zpin2);
-  delay(1);
+  //delay(1);
 
   x3 = analogRead(xpin3);
-  delay(1);
+  //delay(1);
   y3 = analogRead(ypin3);
-  delay(1);
+  //delay(1);
   z3 = analogRead(zpin3);
 }
 
 void createMessage() {
   //Create messageStr char[]
   //A0
-  itoa(x0Rx, x0Char, 10);    //convert int to char[]
+  itoa(x0, x0Char, 10);    //convert int to char[]
   strcat(messageStr, x0Char);
   strcat(messageStr, ",");
-  itoa(y0Rx, y0Char, 10);
+  itoa(y0, y0Char, 10);
   strcat(messageStr, y0Char);
   strcat(messageStr, ",");
-  itoa(z0Rx, z0Char, 10);
+  itoa(z0, z0Char, 10);
   strcat(messageStr, z0Char);
   strcat(messageStr, ",");
 
   //A1
-  itoa(x1Rx, x1Char, 10);    //convert int to char[]
+  itoa(x1, x1Char, 10);    //convert int to char[]
   strcat(messageStr, x1Char);
   strcat(messageStr, ",");
-  itoa(y1Rx, y1Char, 10);
+  itoa(y1, y1Char, 10);
   strcat(messageStr, y1Char);
   strcat(messageStr, ",");
-  itoa(z1Rx, z1Char, 10);
+  itoa(z1, z1Char, 10);
   strcat(messageStr, z1Char);
   strcat(messageStr, ",");
 
   //A2
-  itoa(x2Rx, x2Char, 10);    //convert int to char[]
+  itoa(x2, x2Char, 10);    //convert int to char[]
   strcat(messageStr, x2Char);
   strcat(messageStr, ",");
-  itoa(y2Rx, y2Char, 10);
+  itoa(y2, y2Char, 10);
   strcat(messageStr, y2Char);
   strcat(messageStr, ",");
-  itoa(z2Rx, z2Char, 10);
+  itoa(z2, z2Char, 10);
   strcat(messageStr, z2Char);
   strcat(messageStr, ",");
 
   //A3
-  itoa(x3Rx, x3Char, 10);    //convert int to char[]
+  itoa(x3, x3Char, 10);    //convert int to char[]
   strcat(messageStr, x3Char);
   strcat(messageStr, ",");
-  itoa(y3Rx, y3Char, 10);
+  itoa(y3, y3Char, 10);
   strcat(messageStr, y3Char);
   strcat(messageStr, ",");
-  itoa(z3Rx, z3Char, 10);
+  itoa(z3, z3Char, 10);
   strcat(messageStr, z3Char);
   strcat(messageStr, ",");
 }
--- 3 - Firmware/2 - Preperation/checksum try.py
+++ 3 - Firmware/2 - Preperation/checksum try.py
@@ -1,6 +1,6 @@
 import array
-message = "10,304,296,362,223,352,324,281,331,356,905,362,361,'"
-byteMessage = array.array('B', message)
+message = "3,339,264,339,325,261,339,316,269,308,350,314,395,339,265,340,325,262,340,315,268,309,351,312,396,".encode()
+byteMessage = array.array('b', message)
 print(byteMessage)
 print(len(byteMessage))
 
@@ -9,16 +9,16 @@ checkSum = 0
 
 while i < (len(byteMessage)-1):
     checkSum ^= int(byteMessage[i])
-    print(checkSum)
-    print(i, ": ")
-    print(message[i], "\n")
+    #print(checkSum)
+    #print(i, ": ")
+    #print(message[i], "\n")
     i += 1
 
 print(checkSum)
 print(chr(checkSum))
-print(hex(checkSum))
+print(message.decode()[len(message)-1])
 
-if chr(checkSum) == message[len(message)-1]:
+if chr(checkSum) == message.decode()[len(message)-1]:
     print('Correct')
     #Store data into buffer
 else:
